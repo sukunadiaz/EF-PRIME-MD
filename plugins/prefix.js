b76d3def1a439a747d027b73b76c892122f7ba47
Frank Kaumba <engeezgamer@gmail.com>
2025-04-10 19:43:12 +0200

    Update plugins

---
+++ plugins/prefix.js
@@ -0,0 +1,29 @@
+
+
+import config from '../config.cjs';
+
+const prefixCommand = async (m, Matrix) => {
+   
+    if (m.body.toLowerCase() === 'prefix') {
+        const currentPrefix = config.PREFIX;
+        await Matrix.sendMessage(
+            m.from,
+            { 
+                text: `🤖 Current prefix is: *${currentPrefix}*`,
+                contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363419090892208@newsletter',
+                        newsletterName: "EF-PRIME",
+                        serverMessageId: 143
+                    }
+                }
+            },
+            { quoted: m }
+        );
+    }
+};
+
+export default prefixCommand; 
